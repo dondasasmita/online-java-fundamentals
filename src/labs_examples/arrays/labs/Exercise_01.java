5485e6c51bb9f8147ebac7a14071f4187aaa4e5b
dondasasmita <dondasasmita@gmail.com>
2021-01-24 18:51:56 +0800

    basic array exercise

--- src/labs_examples/arrays/labs/Exercise_01.java
+++ src/labs_examples/arrays/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.arrays.labs;
 
+import java.util.Scanner;
+
 /**
  * Arrays calculator
  *
@@ -11,5 +13,27 @@ package labs_examples.arrays.labs;
  */
 
 public class Exercise_01 {
+
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter a number");
+
+        int[] numbers = new int[10];
+
+        for (int i = 0 ; i < 10 ; i++) {
+
+            try {
+                int number = Integer.parseInt(userInput.nextLine());  // Read user input
+                numbers[i] = number;
+                System.out.println("Enter another number");
+
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+
+        }
+
+    }
 
 }
